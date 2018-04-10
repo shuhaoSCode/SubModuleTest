98712d82f28cb9f366a39db0da5f482825667a16
luke <luke.w@topskyweb.com>
2018-04-10 18:16:50 +0800

    Initial commit

---
+++ app/src/main/java/com/example/luke/submoduletest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.luke.submoduletest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
